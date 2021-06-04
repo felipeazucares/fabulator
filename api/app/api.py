4819ace1c5c808ed670a15c1b46bb64e814e12d2
Philip Suggars <phil@red-robot.biz>
2021-06-04 09:56:53 +0100

    Working: Add payload class and storing in tree structure

--- api/app/api.py
+++ api/app/api.py
@@ -3,6 +3,7 @@ from typing import Optional
 from treelib import Node, Tree
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pymongo import MongoClient
 
 # local modules here
 
@@ -28,8 +29,9 @@ def initialise_tree():
 
 
 class payload():
-    def __init__(self, description, prev, next, tags):
+    def __init__(self, description: Optional[str] = None, prev: Optional[str] = None, next: Optional[str] = None, tags: Optional[list] = None, text: Optional[str] = None):
         self.description = description
+        self.text = text
         self.prev = prev
         self.next = next
         self.tags = tags
@@ -42,16 +44,19 @@ async def treeDump() -> dict:
 
 
 @app.post("/nodes/{name}")
-async def create_new(name: str, parent_node: Optional[str] = None) -> dict:
+async def create_new(name: str, parent_node: Optional[str] = None, description: Optional[str] = None, prev: Optional[str] = None, next: Optional[str] = None, tags: Optional[list] = None, text: Optional[str] = None) -> dict:
     # generate a new id for the node if we have a parent
+
+    node_payload = payload(description=description,
+                           prev=prev, next=next, tags=tags, text=text)
     if parent_node:
         new_node = tree.create_node(
-            name, parent=parent_node)
+            name, parent=parent_node, data=node_payload)
     else:
         # No parent so check if we already have a root
         if tree.root == None:
             new_node = tree.create_node(
-                name)
+                name, data=node_payload)
         else:
             return {"message": "Tree already has a root node"}
 
