ae275c1b598e2baf6f7d823eab85703c5847195e
Philip Suggars <phil@red-robot.biz>
2021-06-10 21:16:13 +0100

    Working: Added boiler plate to the databases.py module

--- server/app/database.py
+++ server/app/database.py
@@ -1,24 +1,24 @@
 import motor.motor_asyncio
-client = motor.motor_asyncio.AsyncIOMotorClient()
+from bson.objectid import ObjectId
+
+MONGO_DETAILS = "mongodb://localhost:27017"
+
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
+
+database = client.students
+
+nodes_collection = database.get_collection("nodes_collection")
 
 
-async def setup_db():
-    db = AsyncIOMotorClient().test
-    await db.pages.drop()
-    html = '<html><body>{}</body></html>'
-    await db.pages.insert_one({'_id': 'page-one',
-                               'body': html.format('Hello!')})
-
-    await db.pages.insert_one({'_id': 'page-two',
-                               'body': html.format('Goodbye.')})
-
-    return db
+# helpers
 
 
-async def do_insert():
-    db = client()['test_database']
-    collection = db['test_collection']
-    document = {'key': 'value'}
-    result = await db.test_collection.insert_one(document)
-    print('result %s' % repr(result.inserted_id))
-    return {result}
+def student_helper(student) -> dict:
+    return {
+        "id": str(student["_id"]),
+        "fullname": student["fullname"],
+        "email": student["email"],
+        "course_of_study": student["course_of_study"],
+        "year": student["year"],
+        "GPA": student["gpa"],
+    }
