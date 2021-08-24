309be10dc2e9db6aaf6df4d633028c5f61963cf5
FelipeAzucares <phil@red-robot.biz>
2021-08-24 21:40:50 +0100

    Working: Example jwt code is working

--- server/app/api.py
+++ server/app/api.py
@@ -68,7 +68,10 @@ app.add_middleware(
 # ------------------------
 #      example security code
 # ------------------------
-
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 fake_users_db = {
@@ -114,14 +117,14 @@ def get_password_hash(password):
     return pwd_context.hash(password)
 
 
-def get_user(db, username: str):
+def get_user_func(db, username: str):
     if username in db:
         user_dict = db[username]
         return UserInDB(**user_dict)
 
 
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
+def authenticate_user(db, username: str, password: str):
+    user = get_user_func(db, username)
     if not user:
         return False
     if not verify_password(password, user.hashed_password):
@@ -154,7 +157,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
+    user = get_user_func(fake_users_db, username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
