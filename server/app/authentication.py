3462c19948f2001bb7a04082041d1c558d9d79c8
FelipeAzucares <phil@red-robot.biz>
2021-12-12 12:55:24 +0000

    WIP: trying to get app to connect to containerised REDIS

--- server/app/authentication.py
+++ server/app/authentication.py
@@ -1,4 +1,3 @@
-
 # from fastapi.security import OAuth2PasswordBearer
 import os
 from time import tzname
@@ -14,20 +13,18 @@ import app.database as database
 import redis
 
 REDISHOST = os.getenv(key="REDISHOST")
+REDISPORT = os.getenv(key="REDISPORT")
 timezone(tzname[0]).localize(datetime.now())
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-class Authentication():
-
+class Authentication:
     def __init__(self):
 
-        self.SECRET_KEY = os.getenv('SECRET_KEY')
-        self.ALGORITHM = os.getenv('ALGORITHM')
-        self.ACCESS_TOKEN_EXPIRE_MINUTES = int(
-            os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
-        self.user_storage = database.UserStorage(
-            collection_name="user_collection")
+        self.SECRET_KEY = os.getenv("SECRET_KEY")
+        self.ALGORITHM = os.getenv("ALGORITHM")
+        self.ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+        self.user_storage = database.UserStorage(collection_name="user_collection")
 
     def verify_password(self, plain_password, hashed_password):
         return pwd_context.verify(plain_password, hashed_password)
@@ -36,15 +33,17 @@ class Authentication():
         return pwd_context.hash(password)
 
     async def get_user_by_username(self, username: str):
-        """ returns the details for a given userid """
-        return(await self.user_storage.get_user_details_by_username(username=username))
+        """returns the details for a given userid"""
+        return await self.user_storage.get_user_details_by_username(username=username)
 
     async def get_user_by_account_id(self, account_id: str):
-        """ returns the details for a given account_id """
-        return(await self.user_storage.get_user_details_by_account_id(account_id=account_id))
+        """returns the details for a given account_id"""
+        return await self.user_storage.get_user_details_by_account_id(
+            account_id=account_id
+        )
 
     async def authenticate_user(self, username: str, password: str):
-        """ passed a db of users & username and input password, verifies password - returns user"""
+        """passed a db of users & username and input password, verifies password - returns user"""
         user = await self.get_user_by_username(username)
         if not user:
             return False
@@ -52,49 +51,55 @@ class Authentication():
             return False
         return user
 
-    def create_access_token(self, data: dict, expires_delta: Optional[timedelta] = None):
-        """ create an access token with an expiry date"""
+    def create_access_token(
+        self, data: dict, expires_delta: Optional[timedelta] = None
+    ):
+        """create an access token with an expiry date"""
         to_encode = data.copy()
         if expires_delta:
             expire = datetime.now(timezone("gmt")) + expires_delta
         else:
-            expire = datetime.now(timezone("gmt")) + \
-                timedelta(minutes=self.ACCESS_TOKEN_EXPIRE_MINUTES)
+            expire = datetime.now(timezone("gmt")) + timedelta(
+                minutes=self.ACCESS_TOKEN_EXPIRE_MINUTES
+            )
         to_encode.update({"exp": expire})
-        encoded_jwt = jwt.encode(
-            to_encode, self.SECRET_KEY, algorithm=self.ALGORITHM)
+        encoded_jwt = jwt.encode(to_encode, self.SECRET_KEY, algorithm=self.ALGORITHM)
         return encoded_jwt
 
     def add_blacklist_token(self, token):
-        """ add the given token to the blacklist"""
+        """add the given token to the blacklist"""
         credentials_exception = HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Could not validate credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
-        r = redis.Redis()
+        redis_client = redis.StrictRedis(host=REDISHOST, port=REDISPORT)
+
         try:
-            payload = jwt.decode(token, self.SECRET_KEY,
-                                 algorithms=[self.ALGORITHM])
+            payload = jwt.decode(token, self.SECRET_KEY, algorithms=[self.ALGORITHM])
             account_id: str = payload.get("sub")
             token_scopes = payload.get("scopes", [])
             expires = payload.get("exp")
-            token_data = TokenData(scopes=token_scopes,
-                                   username=account_id, expires=expires)
+            token_data = TokenData(
+                scopes=token_scopes, username=account_id, expires=expires
+            )
         except ExpiredSignatureError:
             raise credentials_exception
-        result = r.setex(
-            token, int((token_data.expires - datetime.now(timezone('gmt'))).total_seconds()), 1)
-        r.close()
+        result = redis_client.setex(
+            token,
+            int((token_data.expires - datetime.now(timezone("gmt"))).total_seconds()),
+            1,
+        )
+        redis_client.close()
         return result
 
     def is_token_blacklisted(self, token):
-        """ return true if supplied token is in the blacklist"""
+        """return true if supplied token is in the blacklist"""
         # need to create a new instance of aioredis to get pytests to work
-        r = redis.Redis()
-        if(r.get(token)):
-            r.close()
+        redis_client = redis.StrictRedis(host=REDISHOST, port=REDISPORT)
+        if redis_client.get(token):
+            redis_client.close()
             return True
         else:
-            r.close()
+            redis_client.close()
             return False
